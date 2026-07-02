e47da67af24c02b6f98304a728effbed9b851ff6
Esteban1298 <edtv1998@gmail.com>
2026-07-02 15:58:38 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let btnInfo = document.querySelector(".btn-info")
+let imgInfo = document.querySelector(".img-info")
+
+btnInfo.addEventListener("click",function(){
+    document.body.classList.toggle("info-active")
+})
+
